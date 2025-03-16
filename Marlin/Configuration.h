85fc959e25710cc988e177bd0c6506eb2a8f148c
Jason Klapste <jklapste@gmail.com>
2025-03-16 12:36:50 -0500

    comment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,6 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
+// Don't want to drop this but it removing STRING_CONFIG_H_AUTHOR saves us some firmware space
 //#define STRING_CONFIG_H_AUTHOR "Thomas Toka fork" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
